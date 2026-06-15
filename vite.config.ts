bf7539b80d539b8fb294b1367793c56dd12a5e6c
piriyanvijayanantha <piriyan04@outlook.com>
2026-06-16 00:28:05 +0200

    feat: update tsconfig and vite config for new components and base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ändern zu '/datemate/' falls du ein Subrepo nutzt
+  base: '/datemate/',
 })
